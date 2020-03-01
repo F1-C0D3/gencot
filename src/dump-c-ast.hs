972edd667715312127fe35359acc0b42af2c537f
Gunnar Teege <gunnar.teege@unibw.de>
2020-03-01 08:53:35 +0100

    copy dump-c-ast from brancxh nesttags

---
+++ src/dump-c-ast.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE PackageImports #-}
+module Main where
+
+import Language.C.Data.Ident
+import Language.C.Analysis
+import Language.C.Analysis.DefTable (globalDefs)
+
+import Gencot.Input (readFromInput_,getOwnDeclEvents)
+import Gencot.Text.DumpCAst (dumpGlobals)
+
+main :: IO ()
+main = do
+    {- parse and analyse C source and get global definitions -}
+    table <- readFromInput_
+    {- dump global declarations and definitions -}
+    let lines = dumpGlobals $ getOwnDeclEvents (globalDefs table) constructFilter
+    {- Output -}
+    putStrLn $ unlines lines
+
+constructFilter :: DeclEvent -> Bool
+--constructFilter (TagEvent (EnumDef (EnumType (AnonymousRef _) _ _ _))) = False
+--constructFilter (DeclEvent (Declaration _)) = False
+constructFilter _ = True
+ 

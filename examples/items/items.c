7e69e850c4dbee1fde26c887096ade28d245cf1d
Gunnar Teege <gunnar.teege@unibw.de>
2020-05-08 10:04:06 +0200

    add properties ro ar ns nn to manual and example

--- examples/items/items.c
+++ examples/items/items.c
@@ -4,7 +4,7 @@
 intP_t allocInt(void);
 struct s *allocArr(void);
 
-void printAlloc(intCP_t p1, intCA_t p2) {
+static void printAlloc(intCP_t p1, intCA_t p2) {
     printf("p1 points to %d\n",*p1);
     printf("p2 contains: %d, %d, %d\n", p2[0], p2[1], p2[2]);
 }

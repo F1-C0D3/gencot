c6962c77e5a368940fca4ed63036335c39742b7d
Gunnar Teege <gunnar.teege@unibw.de>
2020-05-15 17:20:47 +0200

    use external items in gencot-translate

--- src/gencot-translate.hs
+++ src/gencot-translate.hs
@@ -10,6 +10,7 @@ import Language.C.Analysis.DefTable (globalDefs)
 
 import Gencot.Input (readFromInput_,getOwnDeclEvents)
 import Gencot.Items.Properties (readPropertiesFromFile)
+import Gencot.Items.Identifier (getTypedefNames)
 import Gencot.Traversal (runFTrav)
 import Gencot.Cogent.Output (prettyTopLevels)
 import Gencot.Cogent.Translate (transGlobals)
@@ -18,16 +19,20 @@ main :: IO ()
 main = do
     {- get and test arguments -}
     args <- getArgs
-    when (length args /= 2) 
-        $ error "expected arguments: <original source file name> <item properties file name>"
+    when (length args /= 3) 
+        $ error "expected arguments: <original source file name> <item properties file name> <used external items file name>"
     {- get own file name -}
     let fnam = head args
     {- get item property map -}
     ipm <- readPropertiesFromFile $ head $ tail args
+    {- get list of additional external items to process -}
+    useditems <- (liftM ((filter (not . null)) . lines)) (readFile $ head $ tail $ tail args)
     {- parse and analyse C source and get global definitions -}
     table <- readFromInput_
+    {- Determine type names used directly in the Cogent compilation unit -}
+    let unitTypeNames = getTypedefNames useditems
     {- translate global declarations and definitions to Cogent -}
-    toplvs <- runFTrav table (fnam,ipm,(False,[])) $ transGlobals $ getOwnDeclEvents (globalDefs table) constructFilter
+    toplvs <- runFTrav table (fnam,ipm,(True,unitTypeNames)) $ transGlobals $ getOwnDeclEvents (globalDefs table) constructFilter
     {- Output -}
     print $ prettyTopLevels toplvs
 

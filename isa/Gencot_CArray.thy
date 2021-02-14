55607a3cf88a66599e0dab822811392d81724b9d
Gunnar Teege <gunnar.teege@unibw.de>
2021-02-14 10:03:35 +0100

    improve and fix shallow embedding
    of array and pointer operations

--- isa/Gencot_CArray.thy
+++ isa/Gencot_CArray.thy
@@ -18,13 +18,21 @@ definition setArr' :: "('n::finite, 'el) FixedList \<Rightarrow> (32 word, 'el) 
         if unat idx >= CARD('n) then lst
         else (Abs_FixedList (list_update (Rep_FixedList lst) (unat idx) elm))"
 
+definition modifyArr' :: "('n::finite, 'el) FixedList \<Rightarrow> (32 word, ('el, 'arg) RR \<Rightarrow> ('el, 'arg) RR, 'arg) Tup3 \<Rightarrow> ('n, 'el) FixedList \<times> 'arg"
+  where
+    "modifyArr' lst arg2 \<equiv> 
+        let idx = Tup3.p1\<^sub>f arg2; chf = Tup3.p2\<^sub>f arg2; inp = Tup3.p3\<^sub>f arg2 in
+          if unat idx >= CARD('n) then (lst, inp)
+          else let l = Rep_FixedList lst; elm = nth l (unat idx); chres = chf (RR.make elm inp)
+               in (Abs_FixedList (list_update l (unat idx) (RR.p1\<^sub>f chres)), RR.p2\<^sub>f chres)"
+
 definition modifyArrDflt' :: "('n::finite, 'el) FixedList \<Rightarrow> (32 word, ('el, 'arg) RR \<Rightarrow> ('el, 'out) RR, 'arg) Tup3 \<Rightarrow> ('n, 'el) FixedList \<times> 'out"
   where
     "modifyArrDflt' lst arg2 \<equiv> 
         let idx = Tup3.p1\<^sub>f arg2; chf = Tup3.p2\<^sub>f arg2; inp = Tup3.p3\<^sub>f arg2 in
           if unat idx >= CARD('n) then (lst, defaultVal ())
-          else let l = Rep_FixedList lst; elm = nth l (unat idx)
-               in (Abs_FixedList (list_update l (unat idx) (RR.p1\<^sub>f (chf (RR.make elm inp )))), defaultVal ())"
+          else let l = Rep_FixedList lst; elm = nth l (unat idx); chres = chf (RR.make elm inp)
+               in (Abs_FixedList (list_update l (unat idx) (RR.p1\<^sub>f chres)), RR.p2\<^sub>f chres)"
 
 text \<open>
   This locale has the wrapper record access and constructor as parameters.
@@ -38,6 +46,9 @@ definition getArrFxd :: "('arr, 32 word) RR \<Rightarrow> 'el"
 definition setArrFxd :: "('arr, (32 word, 'el) RR) RR \<Rightarrow> ('arr, unit) RR"
   where "setArrFxd arg \<equiv>
   let arrlst = fld (RR.p1\<^sub>f arg); arg2 = RR.p2\<^sub>f arg in RR.make (wrp(setArr' arrlst arg2)) ()"
+definition modifyArrFxd :: "('arr, (32 word, ('el, 'arg) RR \<Rightarrow> ('el, 'arg) RR, 'arg) Tup3) RR \<Rightarrow> ('arr, 'arg) RR"
+  where "modifyArrFxd arg \<equiv>
+  let arrlst = fld (RR.p1\<^sub>f arg); arg2 = RR.p2\<^sub>f arg; (reslst,res) = modifyArr' arrlst arg2 in RR.make (wrp reslst) res"
 definition modifyArrDfltFxd :: "('arr, (32 word, ('el, 'arg) RR \<Rightarrow> ('el, 'out) RR, 'arg) Tup3) RR \<Rightarrow> ('arr, 'out) RR"
   where "modifyArrDfltFxd arg \<equiv>
   let arrlst = fld (RR.p1\<^sub>f arg); arg2 = RR.p2\<^sub>f arg; (reslst,res) = modifyArrDflt' arrlst arg2 in RR.make (wrp reslst) res"
--- isa/Gencot_CPointer.thy
+++ isa/Gencot_CPointer.thy
@@ -1,7 +1,16 @@
 theory Gencot_CPointer
   imports ShallowShared
+  "HOL-Library.Adhoc_Overloading"
 begin
 
 axiomatization where getPtr_def[simp]: "\<forall>x . getPtr x = CPtr.cont\<^sub>f x" 
+axiomatization where setPtr_def[simp]: "\<forall>arg . setPtr arg = RR.make (CPtr.make (RR.p2\<^sub>f arg)) ()"
+axiomatization where exchngPtr_def[simp]: 
+  "\<forall> arg . exchngPtr arg = RR.make (CPtr.make (RR.p2\<^sub>f arg)) (CPtr.cont\<^sub>f (RR.p1\<^sub>f arg))"
+axiomatization where modifyPtr_def[simp]: 
+  "\<forall> arg . modifyPtr arg = 
+     (let p=RR.p1\<^sub>f arg; chf = RR.p1\<^sub>f (RR.p2\<^sub>f arg); inp = RR.p2\<^sub>f (RR.p2\<^sub>f arg);
+         chres = chf (RR.make (CPtr.cont\<^sub>f p) inp) 
+     in RR.make (CPtr.make (RR.p1\<^sub>f chres)) (RR.p2\<^sub>f chres))"
 
 end

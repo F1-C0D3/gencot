534832d2ed4f45aabc9a310dc2a1ec8927304a41
Gunnar Teege <gunnar.teege@unibw.de>
2021-12-22 21:56:05 +0100

    begin verification documentation

---
+++ doc/verif/Section_prop.thy
@@ -0,0 +1,9 @@
+theory Section_prop
+  imports Section_type
+
+begin
+section "Proving Properties"
+text_raw\<open>\label{verif-prop}\<close>
+
+
+end
---
+++ doc/verif/Section_type.thy
@@ -0,0 +1,9 @@
+theory Section_type
+  imports Section_sem
+
+begin
+section "Datatype Abstraction"
+text_raw\<open>\label{verif-type}\<close>
+
+
+end
